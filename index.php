de6b01c3221887c6ad9165704fac660e99bfdc69
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-05-29 22:45:50 +0530

    Added feedback section

--- index.php
+++ index.php
@@ -137,8 +137,8 @@
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3557.727619630101!2d75.78298191452059!3d26.912138266594702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db40b6ed16f5f%3A0x902e85e7d6dc03fb!2sJai+Mahal+Palace!5e0!3m2!1sen!2sin!4v1558615764072!5m2!1sen!2sin" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
             <div class="col-md-6 contact">
-                <h1 style="color: white; font-family: 'Arial'; text-align: center;">Sign Up</h1>
-                <p style="text-align: center; color: gold;">Sign up to become our customer</p>
+                <h1 style="color: white; font-family: 'Arial'; text-align: center;">Feedback</h1>
+                <p style="text-align: center; color: gold;">Give your feedback</p>
                 
                 <form action="" class="contact-form">
                     <div class="form-group ">
@@ -150,12 +150,8 @@
                         <input type="email" class="form-control" required="" name="email">
                     </div>
                     <div class="form-group">
-                        <lable style="color: gold;">Password : </lable>
-                        <input type="password" class="form-control" required="" name="password">
-                    </div>
-                    <div class="form-group">
-                        <lable style="color: gold;">Phone Number : </lable>
-                        <input type="text" class="form-control" required="" name="phone">
+                        <lable style="color: gold;">Feedback : </lable>
+                        <textarea name="feedback" id="" cols="30" rows="10" class="form-control"></textarea>
                     </div>
                     <div class="form-group">
                     
