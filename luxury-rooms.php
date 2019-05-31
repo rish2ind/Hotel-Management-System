812e4bb6bb5873880b6e4ece2a2d4df28c68ca37
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-05-31 14:22:47 +0530

    Adding details of luxury rooms

---
+++ luxury-rooms.php
@@ -0,0 +1,34 @@
+<?php
+    include('header-other.php');
+?>
+
+
+<div class="container">
+            <div class="row">
+                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Luxury Rooms</h1>
+            </div>
+            <div class="row supirior">
+               <div class="col-md-3">
+                <img src="Images/luxury.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="col-md-9">
+                    <h2 style="color: green;">Facilities</h2><br>
+                    
+<ul style="font-size: 18px;"><li>
+    Free Wifi
+</li>
+<li>TV with 36 Indian and International Channels</li>
+<li>Minibar</li>
+<li>Refrigerator</li>
+<li>Air Condition</li>
+<li>Luxury branded Carpe Diem bed</li>
+<li>Bathtub and Shower</li>
+<li>Cosmetic Mirror in bathroom<li></ul>
+<span style="color: red; font-size:18px;"> Price : &nbsp;6000 /-</span>
+                </div>
+            </div>
+        </div>
+        
+       <?php
+    include('footer.php');
+?>
