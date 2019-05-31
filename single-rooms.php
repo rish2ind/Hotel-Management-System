4d1c2151e17b63429dc0b93384bc7cd4d03fe5d5
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-05-31 14:25:44 +0530

    Adding details of single rooms

---
+++ single-rooms.php
@@ -0,0 +1,30 @@
+<?php
+    include('header-other.php');
+?>
+
+
+<div class="container">
+            <div class="row">
+                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Single Rooms</h1>
+            </div>
+            <div class="row supirior">
+               <div class="col-md-3">
+                <img src="Images/Single.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="col-md-9">
+                    <h2 style="color: green;">Facilities</h2><br>
+                    
+<ul style="font-size: 18px;"><li>
+    Single Bed
+</li>
+<li>Air Conditioned</li>
+<li>Laundry Facilities</li>
+<li>Cable/Satelite TV</li></ul>
+<span style="color: red; font-size:18px;">SINGLE 500 /- </span>
+                </div>
+            </div>
+        </div>
+        
+       <?php
+    include('footer.php');
+?>
