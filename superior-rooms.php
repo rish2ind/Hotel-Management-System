714772707cc218aab0acd30c1357c7859256ab33
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-05-31 14:23:18 +0530

    Adding details of supirior rooms

---
+++ superior-rooms.php
@@ -0,0 +1,26 @@
+<?php
+    include('header-other.php');
+?>
+        
+        
+        <div class="container">
+            <div class="row">
+                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Supirior Rooms</h1>
+            </div>
+            <div class="row supirior">
+               <div class="col-md-3">
+                <img src="Images/Superior.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="col-md-9">
+                    <h2 style="color: green;">Facilities</h2><br>
+                    <p style="text-align: left; font-size: 18px;">
+Inclusive of breakfast, wifi, bottled water.
+Taxes as per applicable. <br>
+<span style="color: red;">SINGLE 2000 /- <br>DOUBLE 2500 /-</span></p>
+                </div>
+            </div>
+        </div>
+        
+       <?php
+        include('footer.php');
+?>
