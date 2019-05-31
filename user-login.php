a15dbfc38345d9aa96f9bf0c05d688752787a3f7
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-05-31 14:28:37 +0530

    Adding php code for sign in and sign up

--- user-login.php
+++ user-login.php
@@ -51,6 +51,32 @@
                 </div>
             </div>
         </div>
+                            <!----------------------------   Sign In         ------------------>
+                            
+                        <?php
+                            include('user-connect.php');
+        
+                        if(isset($_POST['signin'])){
+                            $email = $_REQUEST['email'];
+                            $password = $_REQUEST['password'];
+                            
+                            $log = "select * from user_signup where email = '$email' and password = '$password'";
+                            $run = mysqli_query($conn, $log);
+                            if($run){
+                                echo "<script>
+                                    alert ('Login Successfull');
+                                    window.location.href = 'index.php';
+                                </script>";
+                            }
+                            else{
+                                echo "<script>
+                                    alert ('login failed');
+                                </script>";
+                            }
+                        }
+                
+        ?>
+                            
         <div class="container-fluid" style="background-image: url('Images/jaiselmer1.jpg');
                 background-size: cover;">
               <div class="row register">
@@ -58,7 +84,7 @@
                <h2 style="color: white; text-align: center;">Sign In</h2>
                <p style="color: gold; text-align: center;">Sign in to your account</p>
             <div class="form-container">
-                <form action="" class="contact-form">
+                <form action="" class="contact-form" method="post">
                     
                     <div class="form-group">
                         <lable style="color: gold;">Email : </lable>
@@ -78,12 +104,12 @@
                   </div>
                   
                         <!-----------------------------   Sign Up    ------------------------->
-                        
+                 
                         
                 <div class="col-md-6 ">
                    <h2 style="color: white; text-align: center;">Sign Up</h2>
                        <p style="text-align: center; color: gold;">Sign up to become our customer</p>
-                    <form action="" class="contact-form">
+                    <form class="contact-form" method="POST">
                     <div class="form-group ">
                         <lable style="color: gold;">Full Name : </lable>
                         <input type="text" class="form-control" required="" name="name">
@@ -98,13 +124,37 @@
                     </div>
                     <div class="form-group">
                         <lable style="color: gold;">Phone Number : </lable>
-                        <input type="text" class="form-control" required="" name="phone">
+                        <input type="text" class="form-control" required="" name="contact">
                     </div>
                     <div class="form-group">
                     
                         <input type="submit" class="btn btn-info" value="Sign Up" name="signup" style="opacity: 1; margin-left: 40%;">
                     </div>
                 </form>
+                       <?php
+                        include('user-connect.php');
+                  
+                        if(isset($_POST['signup'])){
+                            $uname = $_REQUEST['name'];
+                            $uemail = $_REQUEST['email'];
+                            $upass = $_REQUEST['password'];
+                            $ucontact = $_REQUEST['contact'];
+                            
+                            $sql = "insert into user_signup (name, email, password, contact) values('$uname','$uemail','$upass','$ucontact')";
+                            
+                            $res = mysqli_query($conn, $sql);
+                            if($res){
+                                echo "<script>alert ('Welcome to the hotel !');
+                                 window.location.href = 'index.php';</script>";
+                            }
+                            else{
+                                echo "<script> 
+                                    alert ('Failed');
+                                </script>";
+                            }
+                        }
+                        
+                  ?>
                 </div>  
              </div>       
         </div>
