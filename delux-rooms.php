f4e7245d663489d87fd5d5ca2668d80a2816150f
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-05-31 14:22:19 +0530

    Adding details of delux rooms

---
+++ delux-rooms.php
@@ -0,0 +1,31 @@
+<?php
+    include('header-other.php');
+
+?>
+
+
+ <div class="container">
+            <div class="row">
+                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Delux Rooms</h1>
+            </div>
+            <div class="row supirior">
+               <div class="col-md-3">
+                <img src="Images/delux.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="col-md-9">
+                    <h2 style="color: green;">Facilities</h2><br>
+                    
+<ul style="font-size: 18px;"><li>
+    In room tea and coffee.
+</li>
+<li>Writing desk</li>
+<li>Minibar</li>
+<li>Refrigerator</li></ul>
+<span style="color: red; font-size:18px;">SINGLE 3000 /- &nbsp;&nbsp;DOUBLE 3500 /-</span>
+                </div>
+            </div>
+        </div>
+        
+       <?php
+    include('footer.php');
+?>
