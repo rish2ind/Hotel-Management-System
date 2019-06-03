38c6e1ad7e5c25af749ca16be4503e0572270ccc
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-06-03 11:45:46 +0530

    Adding booking section

--- superior-rooms.php
+++ superior-rooms.php
@@ -5,21 +5,28 @@
         
         <div class="container">
             <div class="row">
-                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Supirior Rooms</h1>
+                <h1 style="float: left; text-shadow: 2px 3px 15px; background-color: white; padding: 15px;">Standard Rooms</h1>
             </div>
             <div class="row supirior">
                <div class="col-md-3">
                 <img src="Images/Superior.jpg" alt="" class="img-responsive">
                 </div>
                 <div class="col-md-9">
-                    <h2 style="color: green;">Facilities</h2><br>
-                    <p style="text-align: left; font-size: 18px;">
-Inclusive of breakfast, wifi, bottled water.
-Taxes as per applicable. <br>
-<span style="color: red;">SINGLE 2000 /- <br>DOUBLE 2500 /-</span></p>
+                    <h2 style="color: green; text-shadow: 2px 3px 35px;">Facilities</h2><br>
+                   <ul style="font-size: 18px;">
+                       <li>
+                           Breakfast
+                       </li>
+                       <li>Air Condition</li>
+                       <li>Free Wifi</li>
+                       <li>Laundry Facility</li>
+                   </ul>
+<span style="color: red; font-size: 18px;">SINGLE 2000 /- &nbsp;&nbsp;DOUBLE 2500 /-</span>
+               <a href="rooms-booking.php"><button class="btn btn-success" style="padding: 15px; float: right; border-radius: 10px;">Book</button></a>
                 </div>
             </div>
         </div>
+            
         
        <?php
         include('footer.php');
