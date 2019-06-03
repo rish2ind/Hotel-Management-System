e34415461e05d064d18dc78f5ecaea8964c42636
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-06-03 11:41:59 +0530

    Adding booking section

--- single-rooms.php
+++ single-rooms.php
@@ -12,7 +12,7 @@
                 <img src="Images/Single.jpg" alt="" class="img-responsive">
                 </div>
                 <div class="col-md-9">
-                    <h2 style="color: green;">Facilities</h2><br>
+                    <h2 style="color: green; text-shadow: 2px 3px 35px;">Facilities</h2><br>
                     
 <ul style="font-size: 18px;"><li>
     Single Bed
@@ -21,6 +21,7 @@
 <li>Laundry Facilities</li>
 <li>Cable/Satelite TV</li></ul>
 <span style="color: red; font-size:18px;">SINGLE 500 /- </span>
+               <a href="rooms-booking.php"><button class="btn btn-success" style="padding: 15px; float: right; border-radius: 10px;">Book</button></a>
                 </div>
             </div>
         </div>
